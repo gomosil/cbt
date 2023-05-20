82c421c2da09bfc8d2f08026bf706c8fa4dff2e5
Isu Kim <edina00@naver.com>
2023-05-20 13:45:27 +0000

    Work on professor_info API and lecture_list API - feature: work on professor_info endpoint API - feature: work on lecture_list endpoint API - todo: fix bug with 400 error when it comes to request.get_data()

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, Response
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -25,5 +26,222 @@ def login():
     else:
         return Response(status=403)
 
+@app.route('/professor_info', methods=['GET'])
+def professor_info():
+    """
+    A function that deals with endpoint /professor_info (GET)
+    Since this is a testing function, this will return random professor information
+    """
+
+    """
+    data = request.get_json()
+    if data.get('id') == "test":    
+        # This needs to be implemented in the future!!
+        response = {"name": "정우진", "lectures": ["고급모바일실험", "운영체제", "컴퓨터구조"]}
+        return Response(json.dumps(response), status=200)
+    else:
+        response = {"name": "", "lectures": []}
+        return Response(json.dumps(response), status=200)
+    """
+    response = {"name": "정우진", "lectures": ["고급모바일실험", "운영체제", "컴퓨터구조"]}
+    return Response(json.dumps(response), status=200)
+
+@app.route('/lecture_list', methods=['GET'])
+def lecture_list():
+    """
+    A function that deals with endpoint /lecture_info (GET)
+    Since this is a testing function, this will return random data.
+    """
+
+    data = [{
+            'name': "고급모바일실험",
+            'dept': "모바일시스템공학과",
+            'lectured': False,
+            "students": [
+                {
+                    "student_name": "강형철",
+                    "student_id": "32####03",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "김민주",
+                    "student_id": "32####71",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "김수진",
+                    "student_id": "32####99",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "김이수",
+                    "student_id": "32####84",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "김준형",
+                    "student_id": "32####97",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "김지용",
+                    "student_id": "32####88",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "김지훈",
+                    "student_id": "32####56",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "나윤진",
+                    "student_id": "32####83",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "덜거르마",
+                    "student_id": "32####25",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "마이클",
+                    "student_id": "32####39",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "무하마드",
+                    "student_id": "32####93",
+                    "department": "모바일시스템공학과",
+                    "attendance": 1
+                }
+            ]
+        },
+        {
+            'name': "운영체제",
+            'dept': "모바일시스템공학과",
+            'lectured': False,
+            "students": [
+                {
+                    "student_name": "무하마드",
+                    "student_id": "32####93",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "박근남",
+                    "student_id": "32####68",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "박민주",
+                    "student_id": "32####23",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "박범순",
+                    "student_id": "32####15",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "박유빈",
+                    "student_id": "32####80",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "박윤아",
+                    "student_id": "32####68",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "백광렬",
+                    "student_id": "32####67",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "서원형",
+                    "student_id": "32####08",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "손보경",
+                    "student_id": "32####90",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "신우재",
+                    "student_id": "32####42",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "심예린",
+                    "student_id": "32####98",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "양대한",
+                    "student_id": "32####54",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "양윤성",
+                    "student_id": "32####30",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "엄현호",
+                    "student_id": "32####87",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "이라은",
+                    "student_id": "32####87",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "이영주",
+                    "student_id": "32####06",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+                {
+                    "student_name": "임석민",
+                    "student_id": "32####63",
+                    "department": "MSE",
+                    "attendance": 1
+                },
+            ]
+        }
+        
+        
+        ]
+        
+
+    return Response(json.dumps(data), status=200)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
+
